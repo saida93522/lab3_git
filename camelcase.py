9dc0a8a59b96daa1c39f1f01db8954a386c722a5
SAIDA HUSSEN <ct8357ln@go.minneapolis.edu>
2021-08-29 10:25:27 -0500

    Add banner welcome message at start of program

--- camelcase.py
+++ camelcase.py
@@ -10,7 +10,15 @@ def camelcase(sentence):
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
 
+def banner():
+    """ Display program name """
+    message = 'Awesome camelcase program!!'
+    stars = '*' * len(message)
+    print(f'\n{stars} \n{message} \n{stars}')
+
+
 def main():
+    banner()
     sentence = input('Enter your sentence: ')
     output = camelcase(sentence)
     print(output)
